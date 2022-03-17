45db493a1ae9c2eb10e8c75c0d2e60f29309faae
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2022-03-17 21:31:36 +0200

    RSOD Windows XP Horror Edition

---
+++ Source Code/RSODWindowsXPHorrorEdition/RSOD.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace RSODWindowsXPHorrorEdition
+{
+    partial class RSOD
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RSOD));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 442);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // RSOD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RSOD";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RSOD_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Source Code/RSODWindowsXPHorrorEdition/RSOD.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace RSODWindowsXPHorrorEdition
+{
+    public partial class RSOD : Form
+    {
+        SoundPlayer rsod;
+        public void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
+        {
+            //This is Very Important Code... DON'T CHANGE THIS!!! 
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            using (Stream s = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName))
+            using (BinaryReader r = new BinaryReader(s))
+            using (FileStream fs = new FileStream(outDirectory + "\\" + resourceName, FileMode.OpenOrCreate))
+            using (BinaryWriter w = new BinaryWriter(fs))
+                w.Write(r.ReadBytes((int)s.Length));
+        }
+        public static string RandomString(int length)
+        {
+            Random random = new Random();
+            const string chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public RSOD()
+        {
+            Directory.CreateDirectory(@"C:\Temp");
+            Extract("RSODWindowsXPHorrorEdition", @"C:\Temp", "Resources", "RSOD.wav");
+            InitializeComponent();
+            this.Text = RandomString(45);
+            pictureBox1.Dock = DockStyle.Fill;
+            this.Show();
+            pictureBox1.Width = this.Width;
+            pictureBox1.Height = this.Height;
+            pictureBox1.Left = Screen.PrimaryScreen.Bounds.Width - this.Width;
+            pictureBox1.Top = Screen.PrimaryScreen.Bounds.Height - this.Height;
+            rsod = new SoundPlayer(@"C:\Temp\RSOD.wav");
+            rsod.PlayLooping();
+            Cursor.Hide();
+            this.BackColor = Color.DarkRed;
+        }
+
+        private void RSOD_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Thread.Sleep(6000);
+            this.Hide();
+            rsod.Stop();
+            Environment.Exit(0);
+            timer1.Start();
+        }
+    }
+}
